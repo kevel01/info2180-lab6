8adf68519aa225aebf857754c859c53a8912f13e
Kevel Campbell <kevel.campbell@mymona.uwi.edu>
2016-11-12 02:08:06 +0000

    exercise 3

--- request.js
+++ request.js
@@ -1,5 +1,8 @@
 window.onload =function() {
+
+	word =document.getElementsByTagName('input')[0];
 	button =document.getElementsByTagName('input')[1];
+
 	button.onclick =function() {
 		connector(); 
 		searchMeaning();
@@ -11,19 +14,23 @@ function connector() {
 		httprequest =new ActiveXObject('Microsoft.XMLHTTP');
 }
 function searchMeaning() {
-	var url = "request.php?q=definition";
+	var url = "request.php?q="+word.value;
 	httprequest.onreadystatechange = search;
 	httprequest.open("GET", url);
 	httprequest.send();
 }
 function search() {
+	var output =document.getElementById('result');
 	if (httprequest.readyState === XMLHttpRequest.DONE) {
 		if (httprequest.status === 200) {
 		 	meaning = httprequest.responseText;
-		 	alert(meaning);
+		 	if (meaning.length <500) {
+		 		output.innerHTML ='<h3> Result </h3>'+meaning;
+			}
+			else {
+				meaning = output.innerHTML = 'word not found';
+			}
 		}
-		else 
-			meaning ='There was a problem with the request, could not get definition';
 	}
 }
 }
